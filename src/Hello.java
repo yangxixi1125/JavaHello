21d0f581294233a67eb22f90de6e683a7c8d3f4d
喜 杨 <yangxirangwo@outlook.com>
2019-03-22 16:57:44 +0800

    增加数据类型

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 		System.out.println("Hello!!!");
 		System.out.println("qqqq\n\n");
 		System.out.println("ÄãºÃ£¬ÖÐÎÄ£¡£¡£¡");
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
 	}
 
 }
