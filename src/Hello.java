53e598704b5e412c56a46fd721d29c112b170411
喜 杨 <yangxirangwo@outlook.com>
2019-03-22 22:12:21 +0800

    测试数据类型输出

--- src/Hello.java
+++ src/Hello.java
@@ -1,11 +1,19 @@
 
 public class Hello {
 	public static void main(String[] args) {
+		int apple = 123;
+		double student = 20.301;
 		System.out.println("Hello!!!");
 		System.out.println("qqqq\n\n");
 		System.out.println("你好，中文！！！");
 		System.out.println(Integer.MAX_VALUE);
 		System.out.println(Integer.MIN_VALUE);
+		System.out.println(apple);
+		System.out.println(student);
+		System.out.println(apple + 1);
+		System.out.println(student + 100);
+		System.out.println(apple + "苹果");
+		System.out.println(student + "高度");
 	}
 
 }
